5d596540bb3a31d2cc9cc5bdd046247e82c40eec
Vladimir Varin <sceefw@yandex.ru>
2023-10-09 15:53:00 +0500

    changed background color

--- js/app.js
+++ js/app.js
@@ -7,7 +7,11 @@ const wind = document.getElementById('wind')
 const searchButton = document.getElementById('search-button')
 const weatherInfoContainer = document.querySelector('.weather-info__container')
 const errorDiv = document.querySelector('.error')
+const weatherContainer = document.querySelector('.weather-container')
+const weatherImg = document.getElementById('weather-img')
 
+weatherContainer.style.backgroundColor = "#31353a"
+//background-image: linear-gradient(0deg, #600684 0%, #08cbf7 100%);
 async function checkWeather(cityInput) {
   const response = await fetch(apiUrl + cityInput + `&appid=${apiKey}`)
   const data = await response.json()
@@ -20,22 +24,22 @@ async function checkWeather(cityInput) {
     weatherTemp.textContent = Math.round(data.main.temp) + '°C'
     city.textContent = data.name
     humidity.textContent = data.main.humidity
-    wind.textContent = data.wind.speed + ' km / h'
+    wind.textContent = data.wind.speed.toFixed(1) + ' km / h'
     weatherInfoContainer.style.display = 'block'
     errorDiv.style.display = 'none'
 
     switch (data.weather[0].main) {
       case 'Clouds':
-        document.getElementById('weather-img').src = 'img/cloudy.png'
+        weatherImg.src = 'img/cloudy.png'
         break
       case 'Rain':
-        document.getElementById('weather-img').src = 'img/storm.png'
+        weatherImg.src = 'img/storm.png'
         break
       case 'Drizzle':
-        document.getElementById('weather-img').src = 'img/drizzle.png'
+        weatherImg.src = 'img/drizzle.png'
         break
       case 'Snow':
-        document.getElementById('weather-img').src = 'img/snow.png'
+        weatherImg.src = 'img/snow.png'
         break
     }
   }
